f80f28806f75f01432559b3c17df3698f1834b39
BINDU-N-R <bindu6433@gmail.com>
2021-08-21 20:49:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,6 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
-  if (navigator.geolocation) {
-    this.getPosition()
-    .then((position) => {
-      this.getWeather(position.coords.latitude, position.coords.longitude);
-    })
-    .catch((err) => {
-      this.getWeather(28.67, 77.22);
-      alert(
-        "You have disabled location service."
-      );
-    });
-} else {
-  alert("Geolocation not available");
-}
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
